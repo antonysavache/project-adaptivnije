230a555bcead3335f9055077c019a90013ab0e79
Istomina Alena <mynikisalena@gmail.com>
2021-10-19 02:01:27 +0300

    добавляет кривую карусель для секции

---
+++ src/js/reviews.js
@@ -0,0 +1,9 @@
+$('.review-list').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots:true,
+        focusOnSelect: true,
+      centerMode: true,
+      });
