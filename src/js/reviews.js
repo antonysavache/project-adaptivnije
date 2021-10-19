acf26cda679a2694c892d9bd5d93e3eeb127f4f4
Istomina Alena <mynikisalena@gmail.com>
2021-10-19 13:08:08 +0300

    Добавляет полное оформление для секции Reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,9 +1,27 @@
-$('.review-list').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 3,
+$('.review-list ').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.review-text-list',
+  dots: false,
+  centerMode: true,
+  centerPadding: '0',
+  focusOnSelect: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        dots:true,
-        focusOnSelect: true,
-      centerMode: true,
-      });
+      }
+    },
+  ]
+
+});
+$('.review-text-list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  asNavFor: '.review-list'
+});
