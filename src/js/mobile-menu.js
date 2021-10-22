b4ed6b74fba7880d8d9dd16f31dd2b9afaacc3f1
antonysavache <89783847+antonysavache@users.noreply.github.com>
2021-10-22 11:07:52 +0300

    upd

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,16 +1,22 @@
-
-   
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleModal);
-    refs.closeMenuBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.menu.classList.toggle("is-open");
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    mobileBtnLinks: document.querySelectorAll("[data-item]")
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+  }
+
+refs.mobileBtnLinks.forEach((element) => {
+  element.addEventListener('click', function() {
+    refs.menu.classList.remove('is-open');
+    // menuBtnRef.classList.remove('is-open');
+  });
+})
+})();
